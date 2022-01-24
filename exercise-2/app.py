bda5e141a5636004557b7bb5584c8e3ad6b88cdb
David <davedogg323@gmail.com>
2022-01-24 14:15:01 -0800

    completed exercise 2

--- exercise-2/app.py
+++ exercise-2/app.py
@@ -1,9 +1,11 @@
 import os
+from unittest import result
 import requests
 
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, send_file
+from geopy.geocoders import Nominatim
 
 
 ################################################
@@ -33,6 +35,13 @@ def get_letter_for_units(units):
     """Returns a shorthand letter for the given units."""
     return 'F' if units == 'imperial' else 'C' if units == 'metric' else 'K'
 
+def get_lat_lon(city_name):
+    geolocator = Nominatim(user_agent='Weather Application')
+    location = geolocator.geocode(city_name)
+    if location is not None:
+        return location.latitude, location.longitude
+    return 0, 0
+
 @app.route('/results')
 def results():
     """Displays results for current weather conditions."""
@@ -40,18 +49,21 @@ def results():
     units = request.args.get('requested_units')
 
     url = 'http://api.openweathermap.org/data/2.5/weather'
+
+    latitude,longitude = get_lat_lon(city)
     params = {
         'appid': API_KEY,
-        'place': city,
+        'lat' : latitude,
+        'lon' : longitude,
         'units': units
     }
     result_json = requests.get(url, params=params).json()
 
     context = {
         'date': datetime.now(),
-        'city': result_json['name'],
+        'city': city,
         'description': result_json['weather'][0]['description'],
-        'temp': result_json['main']['temperature'],
+        'temp': result_json['main']['temp'],
         'humidity': result_json['main']['humidity'],
         'wind_speed': result_json['wind']['speed'],
         'units_letter': get_letter_for_units(units)
